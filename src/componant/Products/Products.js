9be9f3ad0bf4bf93e4ecfa9cd346c0d65a252ded
NaimurRahamanDurjoy <dnaimurrahaman@gmail.com>
2023-07-23 00:52:55 +0600

    Product & Details

--- src/componant/Products/Products.js
+++ src/componant/Products/Products.js
@@ -7,9 +7,11 @@ const Products = (props) => {
         <div className='products-container'>
             <div className="product">
                 <img src= {img} alt={name} />
-
             </div>
             <div className="product-details">
+                <h3 className='product-name'>{name}</h3>
+                <p>Price: ${price}</p>
+                <button className='addToCart'>Add To Cart</button>
 
             </div>
         </div>
