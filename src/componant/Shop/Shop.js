5ccb931838de2d6716a3c1993a87e9fd13c915ec
NaimurRahamanDurjoy <dnaimurrahaman@gmail.com>
2023-08-12 02:00:00 +0600

    Add search field

--- src/componant/Shop/Shop.js
+++ src/componant/Shop/Shop.js
@@ -7,10 +7,14 @@ import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+    const [displayProducts, setDisplayProducts] = useState([]);
     useEffect(() => {
         fetch('./products.JSON')
         .then(res => res.json())
-        .then(data => setProducts(data))
+        .then(data => {
+            setProducts(data);
+            setDisplayProducts(data);
+        })
     } , []);
 
     // console.log(products)
@@ -19,16 +23,22 @@ const Shop = () => {
         setCart(newCart);
         addToDb(product.id);
     }
+    // Display products from search field
+    const handleDisplayProducts = (event) =>{
+        const searchProduct = event.target.value;
+        const matchedProducts = products.filter(product => product.name.toLowerCase().includes(searchProduct.toLowerCase()));
+        setDisplayProducts(matchedProducts);
+    }
 
     useEffect( () => {
         if(products.length){
             const savedProducts = getShoppingCart();
             let storedProduct = [];
             for(const id in savedProducts){
-                const matchedProducts = products.filter(product => product.id === id);
+                const existedProducts = products.find(product => product.id === id);
                 const quantity = savedProducts[id];
-                matchedProducts[0].quantity = quantity;
-                storedProduct.push(matchedProducts[0]);
+                existedProducts.quantity = quantity;
+                storedProduct.push(existedProducts);
             }
             setCart(storedProduct);
         }
@@ -38,12 +48,12 @@ const Shop = () => {
 
         <>
             <div className="search-product">
-                <input type="text" placeholder='Search Product' />
+                <input onChange={handleDisplayProducts} type="text" placeholder='Search Product' />
             </div>
             <div className='shop-container'>
                 <div className="product-container">
                     {
-                        products.map(product => <Products 
+                        displayProducts.map(product => <Products 
                             key = {product.id}
                             product = {product}
                             handleAddToCart = {handleAddToCart}
